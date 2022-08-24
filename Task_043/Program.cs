537f7ab23b8bff265115495767c8e53bffe066ae
NikitaKoptelov <nikita.koptelov2018@yandex.ru>
2022-08-24 21:55:46 +0800

    решение задач

---
+++ Task_043/Program.cs
@@ -0,0 +1,40 @@
+﻿//        Задача:
+//   Написать программу преобразования десятичного числа в двоичное
+//         Программа:
+
+
+string GetNubTwo (int nub)
+{
+    string bitNub = string.Empty;
+    int nubTwo = 0, nub1 = nub, nub2 = nub;
+    do
+    {
+        bitNub += " ";
+        nubTwo = nub1 / 2;
+        nub2 = nubTwo;
+        if (nub2 * 2 == nub1)
+        {
+            bitNub += "0";
+        }
+        else
+        {
+            bitNub += "1";
+        }
+        nub1 = nub2;
+        
+        
+    } while (nub1 > 0);
+    char[] bitNub1 = bitNub.ToCharArray();
+    Array.Reverse(bitNub1);
+    bitNub = new string(bitNub1);
+    return bitNub;
+}
+
+
+
+
+
+
+Console.Write("введите чесло для преобразования - ");
+int nub = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetNubTwo(nub));
