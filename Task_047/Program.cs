6a87f0697a86a72efb594221a259e6b46dc76eaa
NikitaKoptelov <nikita.koptelov2018@yandex.ru>
2022-08-24 22:09:10 +0800

    решение задач

---
+++ Task_047/Program.cs
@@ -0,0 +1,48 @@
+﻿//     Задача:
+//  Написать программу копирования массива
+//      Программа:
+
+
+int GetOldArray (int index)
+{
+    int[] oldArray = {45, 1, 21, 15, 74, 65, 23, 32};
+    return oldArray[index];
+}
+
+
+int GetNewArray (int index)
+{
+    int[] newArray = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        newArray[i] = GetOldArray(i);
+    }
+    return newArray[index];
+}
+
+
+
+void PrintOldArray ()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{GetOldArray(i)} ");
+    }
+}
+
+void PrintNewArray ()
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write($"{GetNewArray(i)} ");
+    }
+}
+
+
+Console.Write("старый массив - ");
+PrintOldArray();
+Console.WriteLine();
+Console.Write("копировать массив: нажмите ввод.");
+Console.ReadLine();
+Console.Write("новый массив - ");
+PrintNewArray();
